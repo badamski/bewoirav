1f42fab8858aefeecad53d4153bf2a8cb9e7433d
badamski <adamski.bartlomiej@gmail.com>
2014-04-07 16:40:39 +0200

    changes

--- MyTecBits-MVC5-Bootstrap3/Models/VarioObject.cs
+++ MyTecBits-MVC5-Bootstrap3/Models/VarioObject.cs
@@ -10,6 +10,14 @@ namespace MyTecBits_MVC5_Bootstrap3.Models
         public int Id { get; set; }
         public string ObjectName { get; set; }
         public string Filename { get; set; }
-        public string Owner { get; set; }
+        public string DateCreated { get; set; }
+        public string Client { get; set; }
+        public string Value { get; set; }
+        public string ValueNetto { get; set; }
+        public string Deadline { get; set; }
+        public string WorkflowState { get; set; }
+        public string EditState { get; set; }
+        public string ClientAddress { get; set; }
+        public string Number { get; set; }
     }
 }
